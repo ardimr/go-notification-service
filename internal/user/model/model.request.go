57f29b448bdf57fe173dffb25f5192b5293f9951
ardimr <rizkyardimaulana@gmail.com>
2023-10-04 22:03:30 +0700

    Updated model

--- internal/user/model/model.request.go
+++ internal/user/model/model.request.go
@@ -18,6 +18,16 @@ type UserOTPVerification struct {
 	Secret  string `json:"secret" binding:"required"`
 }
 
+type UserOTPVerificationParam struct {
+	OTPCode string `form:"otp_code"`
+}
+
 type UserOTPRequest struct {
 	Email string `json:"email" binding:"required,email"`
+}
+
+type OTPVerificationEmailContent struct {
+	Email   string `json:"email" binding:"required,email"`
+	OTPCode string `json:"otp_code" binding:"required"`
+	Url     string `json:"url"`
 }
